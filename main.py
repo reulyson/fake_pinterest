c1fc0a5241983389195d6759fdc81b2acd6b61f3
Reulyson Wanzeler <reulyson@gmail.com>
2025-05-13 14:30:13 -0400

    feat: criar o template inicial do projeto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # criação aplicação
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # rotas
 @app.route('/')
 def index():
-    return 'FakePinterest - Meu primeiro site no ar'
+    return render_template('index.html')
 
 # rodar projeto
 if __name__ == '__main__':
