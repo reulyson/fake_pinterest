20a9b3be508f40eea824fac62467d7ad86a03239
Reulyson Wanzeler <reulyson@gmail.com>
2025-05-13 17:28:33 -0400

    feat: cria um banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 
 # criação aplicação
 app = Flask(__name__)
 
+# criação do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+database = SQLAlchemy(app)
+ 
 # Importa partes do projeto
 from fakepinterest import routes
--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,1 +1,15 @@
 # Cria o banco de dados do site
+from fakepinterest import app, database
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True, nullable=False)
+    nome = database.Column(database.String, nullable=False)
+    senha = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    foto = database.relationship('Foto', backref='usuario', lazy=True)
+
+class Foto(database.Model):
+    id = database.Column(database.Integer, primary_key=True, nullable=False)
+    imagem = database.Column(database.String, default='default.png')
+    data_criacao = database.Column(database.DateTime, nullable=False, default='datetime.utcnow')
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
